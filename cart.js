83d1c2c7d69367d4896857c99cb4ca303a355162
d4wk0m <96721232+d4wk0m@users.noreply.github.com>
2022-03-01 23:11:44 +0100

    Finished website

--- cart.js
+++ cart.js
@@ -15,12 +15,43 @@ var checkout = document.getElementById('checkout');
 var shoescart = document.getElementById("shoes-cart");
 var cartlist = document.getElementById('cartlist');
 var cartilosc = document.getElementById('cartilosc');
+var modal = document.getElementById('modal');
+var closemodal = document.getElementById('closemodal');
+var modalimg = document.getElementById('modalimg');
+var myslide = document.getElementsByClassName('myslideimg');
+var a = myslide[0].src;
+
 
 price.innerText = priceo.innerText;
 carta.style.height = "0px";
 shoescart.style.display = "none";
 temp.style.display = "block";
 checkout.style.display = "none";
+
+for (i = 0; i < myslide.length; i++) {
+    myslide[i].addEventListener("click", function() {
+        a = this.src;
+        console.log(a);
+    })
+}
+
+function openclose(){
+    window.setTimeout(() => {
+        modalimg.src = a;
+        modal.style.display = "flex";
+        console.log(window.screen.height);
+        console.log(window.screen.width);
+        if (window.screen.width >= window.screen.height){
+            modalimg.style.height = "70vh";
+            modalimg.style.width = "70vh";
+        }
+        else{
+            modalimg.style.width = "70vw";
+            modalimg.style.height = "70vw";
+        }
+      }, 10);
+}
+
 
 
 function cart(n){
@@ -39,25 +70,29 @@ cartico.onclick = function(){
     var t = document.getElementById('menu').offsetHeight + 0.02 * screen.width;
     carta.style.top = t + "px";
 
-    if (window.screen.width >= 600){
-        carta.style.height = "60vh";  
+    if (window.screen.width >= 768){
+        
+        carta.style.height = "75vh";   
+    }
+    else if(window.screen.width >= 600){
+        carta.style.height = "60vh";
     }
     else{
         carta.style.height = "50vh";  
     }
 }
 
-cartico.ontouchstart = function(){
-    cartcontainer.style.display = "block"
-    var t = document.getElementById('menu').offsetHeight + 0.02 * screen.width;
-    carta.style.top = t + "px";
-    if (window.screen.width >= 600){
-        carta.style.height = "60vh";  
-    }
-    else{
-        carta.style.height = "50vh";  
-    }  
-}
+// cartico.ontouchstart = function(){
+//     cartcontainer.style.display = "block"
+//     var t = document.getElementById('menu').offsetHeight + 0.02 * screen.width;
+//     carta.style.top = t + "px";
+//     if (window.screen.width >= 600){
+//         carta.style.height = "60vh";  
+//     }
+//     else{
+//         carta.style.height = "50vh";  
+//     }  
+// }
 
 function addtocart(){
     c+=parseInt(quantity.textContent);
@@ -67,8 +102,12 @@ function addtocart(){
         cartcontainer.style.display = "block"
         var t = document.getElementById('menu').offsetHeight + 0.02 * screen.width;
         carta.style.top = t + "px";
-        if (window.screen.width >= 600){
-            carta.style.height = "60vh";  
+        if (window.screen.width >= 768){
+        
+            carta.style.height = "75vh";   
+        }
+        else if(window.screen.width >= 600){
+            carta.style.height = "60vh";
         }
         else{
             carta.style.height = "50vh";  
@@ -121,23 +160,26 @@ window.onclick = function(event) {
             navcontainer.style.display = "none";
           }, 500);
     }
+    else if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
-window.ontouchstart = function(event) {
-    if (event.target == cartcontainer) {
-        carta.style.height = "0px";
-        window.setTimeout(() => {
-            cartcontainer.style.display = "none";
-          }, 500);
-    }
-    else if (event.target == navcontainer){
-        document.getElementById("nav").style.width = "0";
-        document.getElementById("nav").style.paddingLeft = "0";
-        window.setTimeout(() => {
-            navcontainer.style.display = "none";
-          }, 500);
-    }
-}
+// window.ontouchstart = function(event) {
+//     if (event.target == cartcontainer) {
+//         carta.style.height = "0px";
+//         window.setTimeout(() => {
+//             cartcontainer.style.display = "none";
+//           }, 500);
+//     }
+//     else if (event.target == navcontainer){
+//         document.getElementById("nav").style.width = "0";
+//         document.getElementById("nav").style.paddingLeft = "0";
+//         window.setTimeout(() => {
+//             navcontainer.style.display = "none";
+//           }, 500);
+//     }
+// }
 
 
 function openNav() {
